770572423e5fe5efb88c464843d067bfd6543c26
Dario <astorgadm@gmail.com>
2019-04-18 11:58:12 -0300

    add changes node_modules/bulma/css/bulma.min.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// components
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,18 +9,15 @@ import { FilterBoxComponent } from './components/filterallbox/filterallbox.compo
 import { ListAllComponent } from './components/listall/listall.component';
 import { NotFoundComponent } from './components/notfound/notfound.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
 // Services
-
 import { PropertyService } from './services/property.service/property.service';
-
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { from } from 'rxjs';
 
 library.add(fas);
-
 
 @NgModule({
   declarations: [
